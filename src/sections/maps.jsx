f383b26e9007e43810b7bf82c379e772159c6bb7
blloyd08 <blloyd_2004@hotmail.com>
2020-01-12 18:10:51 -0800

    Add maps

--- src/sections/maps.jsx
+++ src/sections/maps.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Navbar, Nav, Card } from 'react-bootstrap';
+
+import CustomsMap from '../maps/customs.png'
+import InterchangeMap from '../maps/interchange.jpg'
+import FactoryMap from '../maps/factory.png'
+import WoodsMap from '../maps/woods.jpg'
+import ShorelineMap from '../maps/shoreline.jpg'
+
+const maps = {
+    Customs: CustomsMap,
+    Interchange: InterchangeMap,
+    Factory: FactoryMap,
+    Woods: WoodsMap,
+    Shoreline: ShorelineMap
+}
+
 
 export function Maps(props){
+    const [currentMap, setMap] = useState("Customs")
+    var currentMapImage = maps[currentMap];
+
+    const handleSelect = eventKey => setMap(eventKey);
+
     return (
-        <div id="maps">
-            {props.children}
-        </div>
+        <>
+            <h1>Maps</h1>
+            <Navbar bg="dark" variant="dark">
+                <Nav bg="dark" variant="pills" defaultActiveKey="Customs" onSelect={handleSelect} >
+                    {Object.keys(maps).map(mapName => {
+                        return (
+                            <Nav.Item>
+                                <Nav.Link eventKey={mapName}>{mapName}</Nav.Link>
+                            </Nav.Item>
+                        );
+                    })}
+                </Nav>
+            </Navbar>
+            <img src={currentMapImage} alt="Map"/>
+        </>
     );
 }
